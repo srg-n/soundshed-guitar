345d6cc78e2513bdbb6f315915f09042ce0a8976
Christopher Cook <christopher.cook@webprofusion.com>
2026-01-23 22:18:41 +0800

    Fix resource copy

--- src/cmake/CopyVst3Bundle.cmake
+++ src/cmake/CopyVst3Bundle.cmake
@@ -1,5 +1,5 @@
-if(NOT DEFINED SOURCE_DLL OR NOT DEFINED DEST_BUNDLE_DIR OR NOT DEFINED RESOURCE_UI_DIR OR NOT DEFINED RESOURCE_AMPS_DIR OR NOT DEFINED RESOURCE_IR_DIR)
-  message(FATAL_ERROR "CopyVst3Bundle.cmake requires SOURCE_DLL, DEST_BUNDLE_DIR, RESOURCE_UI_DIR, RESOURCE_AMPS_DIR, and RESOURCE_IR_DIR.")
+if(NOT DEFINED SOURCE_DLL OR NOT DEFINED DEST_BUNDLE_DIR OR NOT DEFINED RESOURCE_DIR )
+  message(FATAL_ERROR "CopyVst3Bundle.cmake requires SOURCE_DLL, DEST_BUNDLE_DIR, RESOURCE_DIR.")
 endif()
 
 set(contents_dir "${DEST_BUNDLE_DIR}/Contents")
@@ -19,28 +19,10 @@ if(NOT copy_result EQUAL 0)
 endif()
 
 execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E copy_directory "${RESOURCE_UI_DIR}" "${res_dir}/ui"
+  COMMAND "${CMAKE_COMMAND}" -E copy_directory "${RESOURCE_DIR}" "${res_dir}"
   RESULT_VARIABLE res_result
 )
 
 if(NOT res_result EQUAL 0)
   message(WARNING "Failed to copy VST3 resources to bundle (likely in use).")
-endif()
-
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E copy_directory "${RESOURCE_AMPS_DIR}" "${res_dir}/amps"
-  RESULT_VARIABLE amps_result
-)
-
-if(NOT amps_result EQUAL 0)
-  message(WARNING "Failed to copy VST3 amps resources to bundle (likely in use).")
-endif()
-
-execute_process(
-  COMMAND "${CMAKE_COMMAND}" -E copy_directory "${RESOURCE_IR_DIR}" "${res_dir}/ir"
-  RESULT_VARIABLE ir_result
-)
-
-if(NOT ir_result EQUAL 0)
-  message(WARNING "Failed to copy VST3 IR resources to bundle (likely in use).")
 endif()
